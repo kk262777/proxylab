508072cd03fb8d532ff8d216ce8e9523982e8b33
xgy's centos vagrant <xgy1991@gmail.com>
2015-12-08 21:10:56 +0000

    sequential done

--- proxy.c
+++ proxy.c
@@ -8,7 +8,7 @@
 typedef struct request_info {
     char *hostname;
     char *method;
-    int port;
+    char *port;
     char *uri;
 } request_info;
 
@@ -41,7 +41,7 @@ int main(int argc, char **argv)
     	exit(1);
     }
 
-// client length?
+    /* client length?*/
     listenfd = Open_listenfd(argv[1]);
     while(1) {
         clientlen = sizeof(clientaddr)  ;
@@ -53,7 +53,7 @@ int main(int argc, char **argv)
         Close(connfd);
     }
 
-//    printf("%s", user_agent_hdr);
+/*    printf("%s", user_agent_hdr);*/
 }
 
 void doit(int fd) {
@@ -61,7 +61,9 @@ void doit(int fd) {
     char buf[MAXLINE], method[MAXLINE], url[MAXLINE], version[MAXLINE];
     char read_buf;
     rio_t rio;
-    request_info r_info;
+    request_info* r_info;
+
+    r_info = malloc(sizeof(request_info));
 
 /* Accept each request */
     Rio_readinitb(&rio, fd);
@@ -76,7 +78,7 @@ void doit(int fd) {
         return;
     }
 
-    if (!is_valid(fd, method, url, version, &r_info)) {
+    if (!is_valid(fd, method, url, version, r_info)) {
         printf("%s\n", "invalid request");
         return;
     }
@@ -86,7 +88,7 @@ void doit(int fd) {
 
     /* suppose valid here */
     /* make request */
-    handle_connection(fd, &r_info);
+    handle_connection(fd, r_info);
 
     /* make response */
 }
@@ -95,16 +97,17 @@ void handle_connection(int connfd, struct request_info *r_info) {
 
     /* make request */
     int clientfd;
-    char *host, *uri, *port, buf[MAXLINE];
+    int byte_count;
+    char *host, *uri, *port, buf[MAXLINE], out_buf[MAXLINE];
     rio_t rio;
-    port = "";
 
     debugprintf("start connection\n");
     host = r_info->hostname;
-    printf("%s\n", (char *)host);
+    debugprintf("host:%s\n", host);
     uri = r_info->uri;
-    sprintf(port, "%d", r_info->port);
-    GOTCHA;
+    debugprintf("port: %s\n", r_info->port);
+    port = ( (r_info->port)[0] == '\0' ? "80": r_info->port);
+    //printf("port: %s", port);
 
     /* Init client fd towards server */
     debugprintf("start clientfd\n");
@@ -114,15 +117,39 @@ void handle_connection(int connfd, struct request_info *r_info) {
 
     /* Initiate header info */
     sprintf(buf, "%s %s %s\r\n", "GET", uri, "HTTP/1.0");
-    sprintf(buf, "host: %s\r\n\r\n", host);
+    //Rio_writen(clientfd, buf, strlen(buf));
+    if ((r_info->port)[0] == '\0') {
+        sprintf(buf, "%sHost: %s\r\n", buf, host);
+    }
+    else {
+        sprintf(buf, "%sHost: %s:%s\r\n", buf, host, port);
+    }
+    sprintf(buf, "%s%s\r\n", buf, user_agent_hdr);
     Rio_writen(clientfd, buf, strlen(buf));
     //Rio_readlineb(&rio, buf, MAXLINE);
 
-    printf("start read\n");
+    /* Start read and forward */
+    debugprintf("--------------start read--------------\n");
+
+    Rio_readlineb(&rio, buf, MAXLINE);
     while (strcmp(buf, "\r\n")) {
-        Rio_readlineb(&rio, buf, MAXLINE);
+        //sprintf(out_buf, "%s%s", out_buf, buf);
+        Rio_writen(connfd, buf, strlen(buf));
         printf("%s", buf);
+        Rio_readlineb(&rio, buf,MAXLINE);
     }
+
+    //sprintf(out_buf, "%s\r\n", out_buf);
+    sprintf(buf, "\r\n");
+    Rio_writen(connfd, buf, strlen(buf));
+   // Rio_writen(connfd, out_buf, strlen(out_buf));
+    GOTCHA;
+
+
+    while ((byte_count = Rio_readnb(&rio, buf, MAXLINE)) != 0) { 
+        Rio_writen(connfd, buf, byte_count);
+    }
+    Close(clientfd);
     return;
 
 
@@ -133,10 +160,13 @@ void handle_connection(int connfd, struct request_info *r_info) {
 
 int is_valid(int fd, char *method, char *url, char *version, struct request_info *r_info) {
     char hostname[100];
-    int port = 80;
+    char port[20] = "";
     char uri[200];
+    char uri_tmp[200] = "";
     int valid_flag = 0;
-    printf("%s\n", url);
+    int robust_case;
+
+    debugprintf("%s\n", url);
 
     if (strcasecmp(method, "GET")) {
         clienterror(fd, method, "501", "Not Supported method"
@@ -152,27 +182,55 @@ int is_valid(int fd, char *method, char *url, char *version, struct request_info
     }
 
 
-    if (sscanf(url, "http://%99[^:]:%99d/%199[^/n]", hostname, &port, uri) == 3)
+        printf("before:%s\n", port);
+
+    if (sscanf(url, "http://%99[^:]:%20[^/]/%199s", hostname, port, uri_tmp) == 3)
     {
+        debugprintf("case 1");
         valid_flag = 1;
     }
-    else if (sscanf(url, "http://%99[^/]/%199[^/n]", hostname, uri) == 2) {
+    /* http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,800italic,700,800,400*/
+    else if (sscanf(url, "http://%99[^/]/%199s", hostname, uri_tmp) == 2) {
+        debugprintf("case 2");
+        printf("%s\n", port);
+        port[0] = '\0'; 
+        valid_flag = 1;
+    }
+    else if ((sscanf(url, "http://%99[^:]:%20[^/]/", hostname, port ) == 2)) {
+        valid_flag = 1;
+        debugprintf("case 3");
+    }
+    else if (sscanf(url, "http://%99[^/]/", hostname) == 1) {
+        debugprintf("case 4");
+        valid_flag = 1;
+    }
+    /* extreme case */
+    else if ( (robust_case = sscanf(url, "%99[^/]/%199s", hostname, uri_tmp)) > 0) {
+            if (robust_case == 1) {
+                /* set it as empty */
+                uri_tmp[0] = '\0';                
+            }
         valid_flag = 1;
     }
 
+    //printf("sscanf count:%d\n", sscanf(url, "http://%99[^:]:%20[^/]/%199[^/n]", hostname, port, uri_tmp));
+    //printf("sscanf2 count:%d\n", sscanf(url, "http://%99[^/]/%199[^/n]", hostname, uri_tmp));
+
     if (valid_flag) {
-        printf("hostname: %s\n method: %s \n verision: %s\n", hostname, method, version);
-        r_info->port = port;
-        printf("%d\n", port);
-        r_info->hostname = hostname;
-        printf("%s\n", (char *)(r_info->hostname));
-        r_info->uri = uri;
+        debugprintf("method: %s \n verision: %s\n", method, version);
+        r_info->port = strdup(port);
+        debugprintf("port: %s\n", port);
+        r_info->hostname = strdup(hostname);
+        debugprintf("hostname: %s\n", (r_info->hostname));
+        snprintf(uri, sizeof(uri), "%s%s", "/", uri_tmp);
+        r_info->uri = strdup(uri);
+        debugprintf("uri: %s\n", (r_info->uri));
         return 1;
     }
     else {
         printf("%s\n", hostname);
         printf("%s\n", uri);
-        printf("%d\n", port);
+        printf("%s\n", port);
         clienterror(fd, hostname, "400","Bad request", "Please check if the url is valid");
         printf("%s\n", "invalid request: URL");
         return 0;
@@ -195,11 +253,11 @@ void clienterror(int fd, char *cause, char *errnum,
     char buf[MAXLINE], body[MAXBUF];
     Rio_writen(fd, buf, strlen(buf));    Rio_writen(fd, buf, strlen(buf));
     /* Build the HTTP response body */
-    sprintf(body, "<html><title>Tiny Error</title>");
+    sprintf(body, "<html><title>T-Proxy Error</title>");
     sprintf(body, "%s<body bgcolor=""ffffff"">\r\n", body);
     sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
     sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
-    sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
+    sprintf(body, "%s<hr><em>The Tiny Proxy</em>\r\n", body);
 
     /* Print the HTTP response */
     sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
